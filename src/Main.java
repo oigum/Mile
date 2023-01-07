b092d670f1e9f81edaad4457fde063408758be5a
Olesia Igumnova <ipigumo@gmail.com>
2023-01-07 19:57:04 +0300

    BonusMiles

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
-
     public static void main(String[] args) {
-        int ticketPrice = (int) 50_000F;
-
-        int bonusMile = 20;
-        int numberBonus = (int) ticketPrice / bonusMile;
-        System.out.println("Итоговое количество бонусов" + numberBonus);
-
-    }}
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
+}
